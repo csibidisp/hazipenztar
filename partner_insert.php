dad4fa093b9b5392dac2e41736ead0a807325908
csibidisp <disp.salvatore@gmail.com>
2018-09-30 19:46:38 +0200

    data check

--- partner_insert.php
+++ partner_insert.php
@@ -4,6 +4,6 @@ $partner_nev = $_POST['partner_nev'];
 $partner_tipusa = $_POST['partner_tipusa'];
 $partner_allapota = $_POST['partner_allapota'];
 
-
+echo "$partner_nev, $partner_tipusa, $partner_allapota";
 
  ?>

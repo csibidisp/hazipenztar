8405b63f3440f816bb3d4883ccd7a29fdf4548de
csibidisp <disp.salvatore@gmail.com>
2018-09-30 22:03:48 +0200

    error check

--- partner_insert.php
+++ partner_insert.php
@@ -4,8 +4,8 @@ $partner_nev = $_POST['partner_nev'];
 $partner_tipusa = $_POST['partner_tipusa'];
 $partner_allapota = $_POST['partner_allapota'];
 
-//echo "$partner_nev, $partner_tipusa, $partner_allapota";
-
+echo "$partner_nev, $partner_tipusa, $partner_allapota";
+/*
 if(!empty($partner_nev) || !empty($partner_tipusa) || !empty($partner_allapota)) {
 
   //connect database
@@ -46,4 +46,5 @@ if(!empty($partner_nev) || !empty($partner_tipusa) || !empty($partner_allapota))
   echo "All field are required!";
   die();
 }
+*/
 ?>
